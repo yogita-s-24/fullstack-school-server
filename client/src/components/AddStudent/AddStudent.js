d0b7385dd6627255facf5ee1d177577a93e7df2c
yogita-s-24 <yogitashete24@gmail.com>
2023-10-24 14:06:38 +0530

    school server is ready

--- client/src/components/AddStudent/AddStudent.js
+++ client/src/components/AddStudent/AddStudent.js
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
+import './AddStudent.css';
 
 function AddStudent() {
+  const [name,setName] =useState('')
+  const[age, setAge]=useState('')
+  const[mobile,setMobile]=useState('')
+  const[email,setEmail]=useState('')
+
+  const addStudent = async () =>{
+   if(!name || !email || !mobile ||!age ){
+      return alert("All fields are required");
+     
+    }
+
+    const student ={
+      name,
+      age,
+      email,
+      mobile
+    }
+
+    const response = await axios.post('/students',student);
+
+    alert(response.data.message)
+
+    setName('')
+    setAge('')
+    setEmail('')
+    setMobile('')
+  }
+
   return (
-    <div>AddStudent</div>
+    <div>
+      <h1>Add Student</h1>
+      <form>
+        <input type="text" 
+        placeholder='name'
+         className='input-box' 
+         value={name}
+          onChange={(e)=>{
+          setName(e.target.value)
+        }}/>
+
+        <input type="email"
+         placeholder='email'
+          className='input-box' 
+          value={email} 
+          onChange={(e)=>{
+          setEmail(e.target.value)
+        }}/>
+
+        <input type="text"
+         placeholder='Enter Mobile'
+          className='input-box' 
+          value={mobile} 
+          onChange={(e)=>{
+          setMobile(e.target.value)
+        }}/>
+
+        <input type="text"
+         placeholder='Enter Age'
+          className='input-box' 
+          value={age} 
+          onChange={(e)=>{
+          setAge(e.target.value)
+        }}/>
+
+<button type="button" onClick={addStudent}>Add Student</button>
+
+      </form>
+    </div>
   )
 }
 
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Home.css"
+import axios from "axios";
 
 
 function Home() {
+  const [students, setStudents] = useState([]);
+
+  const loadStudents = async () => {
+    const response = await axios.get('/students')
+    setStudents(response.data.data);
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    loadStudents();
+  }, [])
+
   return (
     <>
-    <h1 className='text-center'>All Students</h1>
-    <div class="container">
+      <h1 className='text-center'>All Students</h1>
+      <div className="container">
 
+        {
+          students?.map((student, index)=>{
+const {name, email, age, mobile} = student;
+return (
+  <div>
+  <div key={index} className='student-cards'>
+    <p><b>Name: </b> {name}</p>
+    <p><b>Email: </b> 💌 {email}</p>
+    <p><b>Mobile: </b> {mobile} <b>Age: </b> {age} years old.</p>
     </div>
+    </div>
+    )
+  })
+}
+      </div>
     </>
   )
 }
